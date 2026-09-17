97a945f35d71c9d5c2e96b02b7e175301ecfad74
YudhaAwan <yudha2100019093@webmail.uad.ac.id>
2026-09-17 10:00:42 +0700

    Tambah logo icon.svg dan update favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
-import '../styles/globals.css';
-import { AuthProvider } from '../context/AuthContext';
+import "../styles/globals.css";
+import { AuthProvider } from "../context/AuthContext";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Head>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <title>CloudTrack - Personal Finance App</title>
+      </Head>
       <Component {...pageProps} />
     </AuthProvider>
   );
